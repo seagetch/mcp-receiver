504498444ff36f01fff0b6e7bd25b818f86078b0
seagetch <sigetch@gmail.com>
2022-12-19 03:26:30 +0900

    VMCSender checks packet type.

--- mcp_receiver/vmcsender.py
+++ mcp_receiver/vmcsender.py
@@ -45,9 +45,12 @@ class VMCSender(Runner):
         while True:
             try:
                 data = self.queue.get()
-                for btdt in data["fram"]["btrs"]:
-                    if bone_map[btdt["bnid"]]:
-                        tran = btdt["tran"]
-                        client.send_message("/VMC/Ext/Bone/Pos", [bone_map[btdt["bnid"]], tran[4], tran[5], tran[6], tran[0], tran[1], tran[2], tran[3]])
+                if "skdf" in data:
+                    pass
+                elif "fram" in data:
+                    for btdt in data["fram"]["btrs"]:
+                        if bone_map[btdt["bnid"]]:
+                            tran = btdt["tran"]
+                            client.send_message("/VMC/Ext/Bone/Pos", [bone_map[btdt["bnid"]], tran[4], tran[5], tran[6], tran[0], tran[1], tran[2], tran[3]])
             except KeyError as e:
                 print(e)
