5a25618b5017deef7f2c539fbbc4504ba7600c3d
seagetch <sigetch@gmail.com>
2022-12-19 02:51:32 +0900

    Updated packet handling for two packet types.

--- mcp_receiver/receiver.py
+++ mcp_receiver/receiver.py
@@ -2,17 +2,19 @@ import socket
 import struct
 from mcp_receiver.runner import Runner
 
-data_keys = [b"#", b"head", b"ftyp", b"vrsn", b"sndf", b"ipad", b"rcvp", b"fram", b"fnum", b"time", b"btrs", b"btdt", b"btdt", b"bnid", b"tran", b"skdf", b"bons", b"bndt", b"pbid"]
+def is_field(name):
+    return name.isalpha()
 
 def _deserialize(data, index, length, is_list = False):
     result = [] if is_list else {}
     end_pos = index + length
-    while end_pos - index > 8 and data[index+4:index+8] in data_keys:
+    while end_pos - index > 8 and is_field(data[index+4:index+8]):
         size = struct.unpack("@i", data[index: index+4])[0]
         index += 4
         field = data[index:index+4]
         index += 4
-        value, index = _deserialize(data, index, size, field == b"btrs")
+        value, index2 = _deserialize(data, index, size, field in [b"btrs", b"bons"])
+        index = index2
         if is_list:
             result.append(value)
         else:
@@ -23,17 +25,22 @@ def _deserialize(data, index, length, is_list = False):
     else:
         return result, index
 
-def _process_packet(packet):
-    data = _deserialize(packet, 0, len(packet))[0]
+def _process_packet(data):
     data["head"]["ftyp"] = data["head"]["ftyp"].decode()
     data["head"]["vrsn"] = ord(data["head"]["vrsn"])
-    data["sndf"]["ipad"] = struct.unpack("@II", data["sndf"]["ipad"])
+    data["sndf"]["ipad"] = struct.unpack("@BBBBBBBB", data["sndf"]["ipad"])
     data["sndf"]["rcvp"] = struct.unpack("@H", data["sndf"]["rcvp"])[0]
-    data["fram"]["fnum"] = struct.unpack("@I", data["fram"]["fnum"])[0]
-    data["fram"]["time"] = struct.unpack("@I", data["fram"]["time"])[0]
-    for item in data["fram"]["btrs"]:
-        item["bnid"] = struct.unpack("@H", item["bnid"])[0]
-        item["tran"] = struct.unpack("@fffffff", item["tran"])
+    if "skdf" in data:
+        for item in data["skdf"]["bons"]:
+            item["bnid"] = struct.unpack("@H", item["bnid"])[0]
+            item["pbid"] = struct.unpack("@H", item["pbid"])[0]
+            item["tran"] = struct.unpack("@fffffff", item["tran"])
+    elif "fram" in data:
+        data["fram"]["fnum"] = struct.unpack("@I", data["fram"]["fnum"])[0]
+        data["fram"]["time"] = struct.unpack("@I", data["fram"]["time"])[0]
+        for item in data["fram"]["btrs"]:
+            item["bnid"] = struct.unpack("@H", item["bnid"])[0]
+            item["tran"] = struct.unpack("@fffffff", item["tran"])
     return data
 
 
